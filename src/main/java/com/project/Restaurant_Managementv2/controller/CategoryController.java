77cf8fd7f9970fc9df5c95aa6d2618d13fe21022
Minhduc2712 <115997551+Minhduc2712@users.noreply.github.com>
2023-11-01 02:09:21 +0700

    commit

--- src/main/java/com/project/Restaurant_Managementv2/controller/CategoryController.java
+++ src/main/java/com/project/Restaurant_Managementv2/controller/CategoryController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/categories")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 
 public class CategoryController {
 
